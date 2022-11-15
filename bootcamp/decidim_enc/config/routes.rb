39bd9bb3d70e6f3240ba8e7d78e82cd185a4bc5c
alanB-A <gaba30001@gmail.com>
2022-11-14 23:18:21 -0300

    added project with editable navbars

---
+++ bootcamp/decidim_enc/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :navbars
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  mount Decidim::Core::Engine => '/'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
